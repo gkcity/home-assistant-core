11ec427eca7883183246c3e47a491a2dcef38e5b
ouyangchengfeng <178915185@qq.com>
2026-04-03 10:58:10 +0800

    add components/jd_xiot

---
+++ config/custom_components/jd_xiot/const.py
@@ -0,0 +1,7 @@
+"""Constants for the JingDong XIoT integration."""
+
+DOMAIN = "jd_xiot"
+
+# TODO Update with your own urls
+OAUTH2_AUTHORIZE = "https://www.example.com/auth/authorize"
+OAUTH2_TOKEN = "https://www.example.com/auth/token"
